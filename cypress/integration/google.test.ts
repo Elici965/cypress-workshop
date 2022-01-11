7b7aff0ee625b170e50e606bc369d7960cbfa4c0
Elici965 <95142750+Elici965@users.noreply.github.com>
2022-01-11 11:43:15 -0500

    Update google.test.ts

--- cypress/integration/google.test.ts
+++ cypress/integration/google.test.ts
@@ -2,7 +2,6 @@ describe('This is the first example of cypress', () =>
 {
   it('should have a title', () => {
     cy.visit('https://www.google.com/');
-    debugger;
     cy.title().should('be.equal', 'Google');
   });
 });
