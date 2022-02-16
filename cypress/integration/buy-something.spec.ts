f026de60db7553ad8c702822b736b9c8a7a78638
Elici965 <95142750+Elici965@users.noreply.github.com>
2022-02-16 11:11:57 -0500

    Different describes

--- cypress/integration/buy-something.spec.ts
+++ cypress/integration/buy-something.spec.ts
@@ -2,25 +2,39 @@ import {
   LoginPage, ProductListPage, ShoppingCart, Checkout, CheckoutOverview, OrderSummary
 } from '../page';
 
-  describe('Buy a t-shirt', () => {
-    const loginPage: LoginPage = new LoginPage();
-    const productListPage: ProductListPage = new ProductListPage();
-    const shoppingCartPage: ShoppingCart = new ShoppingCart();
-    const checkout:Checkout = new Checkout();
-    const checkoutOverview: CheckoutOverview = new CheckoutOverview();
-    const orderSummary: OrderSummary = new OrderSummary();
+describe('Buy something', () => {
+  describe('Given a user is login to Saucedemo', () => {
+    before(() => {
+      const loginPage: LoginPage = new LoginPage();
+      cy.visit('https://www.saucedemo.com/');
+      loginPage.loginUser();
+    })
 
-  it('Then should be bought something', () => {
-    cy.visit('https://www.saucedemo.com/');
-    cy.getCookies();
-    cy.clearCookies();
-    loginPage.loginUser();
-    productListPage.addToCart();
-    productListPage.goShoppingCart();
-    shoppingCartPage.goProceedCheckout();
-    checkout.ingresarDatosComprador();
-    checkout.continueShopping();
-    checkoutOverview.finishShopping();
-    orderSummary.getConfirmOrder().should('have.text', 'THANK YOU FOR YOUR ORDER');
-    });
-});
+    describe('when the user adds a product to the cart', () => {
+      before(() => {
+        const productListPage: ProductListPage = new ProductListPage();
+        productListPage.addToCart();
+        productListPage.goShoppingCart();
+      })
+
+      describe('And continue with the purchase process', () => {
+        before('And continue with the purchase process', () => {
+          const shoppingCartPage: ShoppingCart = new ShoppingCart();
+          const checkout: Checkout = new Checkout();
+          const checkoutOverview: CheckoutOverview = new CheckoutOverview();
+          shoppingCartPage.goProceedCheckout();
+          checkout.EnterBuyerData();
+          checkout.continueShopping();
+          checkoutOverview.finishShopping();
+        })
+
+        describe('Then the order is finished and confirmed', () => {
+          it('Then the order is finished and confirmed', () => {
+            const orderSummary: OrderSummary = new OrderSummary();
+            orderSummary.getConfirmOrder().should('have.text', 'THANK YOU FOR YOUR ORDER');
+          })
+        })
+      })
+    })
+  })
+})
