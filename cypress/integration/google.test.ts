c0c60a17d3ac8ed7dfc5a4e97e715b579efa468e
Elici965 <95142750+Elici965@users.noreply.github.com>
2022-03-16 15:00:50 -0500

    more locators

--- cypress/integration/google.test.ts
+++ cypress/integration/google.test.ts
@@ -7,13 +7,4 @@ describe('This is the first example of cypress', () => {
       .type('Cypress');
       cy.title().should('be.equal', 'Google');
   });
-
-  it('should have a title', () => {
-    cy.fixture('google-response').as('googleSearchResponse');
-    cy.intercept('GET', '**/complete/search**', '@googleSearchResponse');
-    cy.visit('https://www.google.com/');
-    cy.get('[name="q"]')
-    .type('Cypress');
-    cy.title().should('be.equal', 'Google');
-});
 });
