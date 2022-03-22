75681abb5d9cc033c8135ebcfffb0626d7e57ae9
Elici965 <95142750+Elici965@users.noreply.github.com>
2022-03-22 16:21:35 -0500

    Update locators.spec.ts

--- cypress/integration/locators.spec.ts
+++ cypress/integration/locators.spec.ts
@@ -2,23 +2,28 @@ import {
     PersonalInformation   
   } from '../page';
   
-  describe('Fill Form', () => {
+  describe('Fill Form', () => 
+  {
     const personalInformation: PersonalInformation = new PersonalInformation();
-    describe('given a user enters the qavbox page', () => {
+    describe('given a user enters the qavbox page', () => 
+    {
       before(() => {
         cy.visit('https://qavbox.github.io/demo/signup/');
-      })
+    })
     
-    describe('when the user fills the form', () => {
-      before(() => {
+    describe('when the user fills and submits the form', () => {
+      before(() => 
+    {
         personalInformation.fillForm();
-      }) 
+    }) 
 
-    describe('the form is submited.', () => {
-      it('the form is submited.',() => {
-        personalInformation.submitForm()
-     }) 
-  })   
+    describe('then the information is submited.', () => 
+    {
+      it('then the information is submited.',() => 
+      {
+        personalInformation.test();
+      }) 
+    })   
+  }) 
 }) 
 }) 
-})
