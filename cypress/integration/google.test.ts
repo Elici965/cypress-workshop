a8f8a44c9a0e0387fac1005f4b13c981e3839c86
Elici965 <95142750+Elici965@users.noreply.github.com>
2022-03-28 14:25:24 -0500

    Update google.test.ts

--- cypress/integration/google.test.ts
+++ cypress/integration/google.test.ts
@@ -1,10 +1,9 @@
 describe('This is the first example of cypress', () => {
   it('should have a title', () => {
-      cy.fixture('google-response').as('googleSearchResponse');
-      cy.intercept('GET', '**/complete/search**', '@googleSearchResponse');
-      cy.visit('https://www.google.com/');
-      cy.get('[name="q"]')
-      .type('Cypress');
-      cy.title().should('be.equal', 'Google');
+    cy.fixture('google-response').as('googleSearchResponse');
+    cy.intercept('GET', '**/complete/search**', '@googleSearchResponse');
+    cy.visit('https://www.google.com/');
+    cy.get('[name="q"]').type('Cypress');
+    cy.title().should('be.equal', 'Google');
   });
 });
